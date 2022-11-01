42df04fe20ad36745057ce35a294e35d5092ca86
Dan Kamochu <danidyllic@outlook.com>
2022-11-01 12:06:22 -0700

    edit on putchar function

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -14,30 +13,10 @@ void print_binary(unsigned long int n)
 	{
 		if (n >> 1)
 			print_binary(n >> 1);
-		putchar((n & 1) + '0');
+		_putchar((n & 1) + '0');
 	}
 	else
 	{
-		putchar('0');
+		_putchar('0');
 	}
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-print_binary(0);
-printf("\n");
-print_binary(1);
-printf("\n");
-print_binary(98);
-printf("\n");
-print_binary(1024);
-printf("\n");
-print_binary((1 << 10) + 1);
-printf("\n");
-return (0);
 }
